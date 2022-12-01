213cc3840ad09a4d84d0edf840d2735cc04a7ef7
Alicefangxin <alicefangxingu@gmail.com>
2022-11-30 21:24:34 -0800

    search professors entrypoint

--- professors/professors-controller.js
+++ professors/professors-controller.js
@@ -1,16 +1,24 @@
 import axios from "axios";
 
-const base_url = "https://rateprof-api.herokuapp.com/professors";
+const base_url = "https://rateprof-api.herokuapp.com";
 
 const ProfessorsController = (app) => {
     const findAllProfessors = async (req, res) => {
         const name = req.query.name;
         const university = req.query.university;
-        const resp = await axios.get(base_url, {params: {name: name, university: university}});
+        const resp = await axios.get(`${base_url}/professors`, {params: {name: name, university: university}});
+        res.json(resp.data);
+    };
+
+    const searchProfessors = async (req, res) => {
+        const name = req.query.name;
+        const university = req.query.university;
+        const resp = await axios.get(`${base_url}/searchprofessors`, {params: {name: name, university: university}});
         res.json(resp.data);
     };
 
   app.get("/professors", findAllProfessors);
+  app.get("/searchprofessors", searchProfessors);
 };
 
 export default ProfessorsController;
