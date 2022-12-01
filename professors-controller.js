26ff111a2a6a5c137aa37fe671fe691b941d5eeb
Nicole Ni <shallwaytalk@gmail.com>
2022-11-30 16:13:06 -0800

    node package - fix axios module not found error

--- professors-controller.js
+++ professors-controller.js
@@ -1,19 +1,20 @@
-import axios from 'axios';
+import axios from "axios";
 
-
-const base_url = "https://rateprof-api.herokuapp.com/professors"
+const base_url = "https://rateprof-api.herokuapp.com/professors";
 // const base_url = "http://localhost:5000/professors"
 
 const ProfessorsController = (app) => {
-    const findAllProfessors = async (req, res) => {
-        const name = req.query.name;
-        const university = req.query.university;
+  const findAllProfessors = async (req, res) => {
+    const name = req.query.name;
+    const university = req.query.university;
 
-        const resp = await axios.get(base_url, {params: {name: name, university: university}});
-        res.json(resp.data);
-    };
+    const resp = await axios.get(base_url, {
+      params: { name: name, university: university },
+    });
+    res.json(resp.data);
+  };
 
-    app.get("/professors", findAllProfessors);
+  app.get("/professors", findAllProfessors);
 };
 
 export default ProfessorsController;
