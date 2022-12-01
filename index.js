3ffce0e7bd29b7cedaece48ebc30dabfe20ee589
Alicefangxin <alicefangxingu@gmail.com>
2022-11-30 19:31:14 -0800

    load users from mongodb

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import express from "express";
 import cors from "cors";
-// import UsersController from "./users/users-controller.js";
-// import mongoose from "mongoose";
-import ProfessorsController from "./professors-controller.js";
+import UsersController from "./users/users-controller.js";
+import mongoose from "mongoose";
+import ProfessorsController from "./professors/professors-controller.js";
 import HelloControllers from "./hello-controllers.js";
 
 const options = {
@@ -18,10 +18,13 @@ const app = express();
 
 // const DB_CONNECT_CRED =
 //   process.env.DB_CONNECT_CRED || "mongodb://localhost:27017/r8Myprof";
-//mongoose.connect(DB_CONNECT_CRED, options);
+
+const DB_CONNECT_CRED="mongodb+srv://cs5610group8:0bDYf9oDxVdWbwSA@cluster0.6n7kodd.mongodb.net/r8MyProf?retryWrites=true&w=majority"
+
+mongoose.connect(DB_CONNECT_CRED, options);
 app.use(cors());
 app.use(express.json());
-// UsersController(app);
+UsersController(app);
 HelloControllers(app);
 ProfessorsController(app);
 app.listen(process.env.PORT || 4000);
