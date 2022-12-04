0bac4810a520845dcc7b9a59f96770d4534aaffd
Nicole Ni <shallwaytalk@gmail.com>
2022-12-04 14:50:53 -0800

    fix tempProfs list bug

--- professors/tempProfs-controller.js
+++ professors/tempProfs-controller.js
@@ -1,20 +1,21 @@
 import * as tempProfsDao from "./tempProfs-dao.js";
 
 const tempProfsController = (app) => {
-    const findAllProfs = async (req, res) => {
-        const profs = await tempProfsDao.findAllProfs()
-        res.json(profs)
-    }
+  const findAllProfs = async (req, res) => {
+    console.log("AAA");
+    const profs = await tempProfsDao.findAllProfs();
+    console.log(profs);
+    res.status(200).json(profs);
+  };
 
-    const findProfById = async(req, res) => {
-        const pid = req.params.pid
-        const prof = await tempProfsDao.findProfById(pid)
-        res.json(prof)
-    }
+  const findProfById = async (req, res) => {
+    const pid = req.params.pid;
+    const prof = await tempProfsDao.findProfById(pid);
+    res.json(prof);
+  };
 
-    app.get('/tempProfs', findAllProfs)
-    app.get('/tempProfs/:pid', findProfById)
-}
-
+  app.get("/tempProfs", findAllProfs);
+  app.get("/tempProfs/:pid", findProfById);
+};
 
 export default tempProfsController;
